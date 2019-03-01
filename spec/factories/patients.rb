ba089ce91b80f6aa79de14be51bd9c9f89fb134e
Shane <x15745561@student.ncirl.ie>
2019-03-01 01:14:27 +0000

    Add post and category factories

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :patient do
-    
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:date_of_birth) { |n| "#{n}/#{n}/#{n}" }
+    sequence(:address) { |n| "Apartment #{n}, Tyrone House" }
+    user
   end
 end
