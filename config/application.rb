b28e78a651c5455c4d263edcef07c7ef95dc61d7
Shane <x15745561@student.ncirl.ie>
2019-03-29 21:10:08 +0000

    set assets.initialize_on_precompile to false for deployment

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
     config.action_controller.include_all_helpers = false
     config.active_record.observers = :patient_observer
